4cb913c73b3d7bf5d39c5fc5d35b02ff587ee1b4
liz-rondone <liz.rondone@northwestern.edu>
2017-09-24 16:25:57 -0500

    messed up the db

--- server.js
+++ server.js
@@ -67,37 +67,6 @@ app.get("/all", function(req, res) {
   });
 });
 
-// 3. At the "/name" path, display every entry in the animals collection, sorted by name
-// app.get("/name", function(req, res) {
-//   // Query: In our database, go to the animals collection, then "find" everything,
-//   // but this time, sort it by name (1 means ascending order)
-//   db.animals.find().sort({ name: 1 }, function(error, found) {
-//     // Log any errors if the server encounters one
-//     if (error) {
-//       console.log(error);
-//     }
-//     // Otherwise, send the result of this query to the browser
-//     else {
-//       res.json(found);
-//     }
-//   });
-// });
-
-// 4. At the "/weight" path, display every entry in the animals collection, sorted by weight
-// app.get("/weight", function(req, res) {
-//   // Query: In our database, go to the animals collection, then "find" everything,
-//   // but this time, sort it by weight (-1 means descending order)
-//   db.animals.find().sort({ weight: -1 }, function(error, found) {
-//     // Log any errors if the server encounters one
-//     if (error) {
-//       console.log(error);
-//     }
-//     // Otherwise, send the result of this query to the browser
-//     else {
-//       res.json(found);
-//     }
-//   });
-// });
 
 // Set the app to listen on port 3000
 app.listen(3000, function() {
