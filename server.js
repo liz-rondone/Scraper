bf3670e35ac2f9760dcd2c7f5287c9bcafc8d0ce
liz-rondone <liz.rondone@northwestern.edu>
2017-09-24 10:24:49 -0500

    adding scraping

--- server.js
+++ server.js
@@ -1,162 +1,95 @@
-/* Showing Mongoose's "Populated" Method
- * =============================================== */
-
 // Dependencies
 var express = require("express");
-var bodyParser = require("body-parser");
--var logger = require("morgan");
-var mongoose = require("mongoose");
-// Requiring our Note and Article models
--var Note = require("./models/Note.js");
--var Article = require("./models/Article.js");
-// Our scraping tools
+var mongojs = require("mongojs");
+// dependencies for scraping
 var request = require("request");
 var cheerio = require("cheerio");
-var exphbs = require("express-handlebars");
-// Set mongoose to leverage built in JavaScript ES6 Promises
-mongoose.Promise = Promise;
-
-// models
-var Article = require('/models/Articles.js');
-var Note = require('models/Note.js');
-
-
 
 // Initialize Express
 var app = express();
 
-// Use morgan and body parser with our app
-app.use(logger("dev"));
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-// Make public a static dir
+// Set up a static folder (public) for our web app
 app.use(express.static("public"));
 
-// Database configuration with mongoose
-mongoose.connect("mongodb://localhost/week18day3mongoose");
-var db = mongoose.connection;
+// Database configuration
+// Save the URL of our database as well as the name of our collection
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
 
-// Show any mongoose errors
+// Use mongojs to hook the database to the db variable
+var db = mongojs(databaseUrl, collections);
+
+// This makes sure that any errors are logged if mongodb runs into an issue
 db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
+  console.log("Database Error:", error);
 });
-
-// Once logged in to the db through mongoose, log a success message
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
-});
-
 
 // Routes
-// ======
+// 1. At the root path, send a simple hello world message to the browser
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
 
-// A GET request to scrape the echojs website
+// scrape data
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with request
-  request("http://www.echojs.com/", function(error, response, html) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(html);
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
-
-      // Save an empty result object
-      var result = {};
-
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children("a").text();
-      result.link = $(this).children("a").attr("href");
-
-      // Using our Article model, create a new entry
-      // This effectively passes the result object to the entry (and the title and link)
-      var entry = new Article(result);
-
-      // Now, save that entry to the db
-      entry.save(function(err, doc) {
-        // Log any errors
-        if (err) {
-          console.log(err);
-        }
-        // Or log the doc
-        else {
-          console.log(doc);
-        }
-      });
-
+    request("https://news.ycombinator.com/", function(error, response, html) {
+        var $ = cheerio.load(html);
+        $(".title").each(function(i, element){
+            var title = $(element).children("a").text();
+            var link = $(element).children("a").attr("href");
+            console.log(title);
+        })
     });
-  });
-  // Tell the browser that we finished scraping the text
-  res.send("Scrape Complete");
 });
 
-// This will get the articles we scraped from the mongoDB
-app.get("/articles", function(req, res) {
-  // Grab every doc in the Articles array
-  Article.find({}, function(error, doc) {
-    // Log any errors
+// 2. At the "/all" path, display every entry in the animals collection
+app.get("/all", function(req, res) {
+  // Query: In our database, go to the animals collection, then "find" everything
+  db.animals.find({}, function(error, found) {
+    // Log any errors if the server encounters one
     if (error) {
       console.log(error);
     }
-    // Or send the doc to the browser as a json object
+    // Otherwise, send the result of this query to the browser
     else {
-      res.json(doc);
+      res.json(found);
     }
   });
 });
 
-// Grab an article by it's ObjectId
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  Article.findOne({ "_id": req.params.id })
-  // ..and populate all of the notes associated with it
-  .populate("note")
-  // now, execute our query
-  .exec(function(error, doc) {
-    // Log any errors
+// 3. At the "/name" path, display every entry in the animals collection, sorted by name
+app.get("/name", function(req, res) {
+  // Query: In our database, go to the animals collection, then "find" everything,
+  // but this time, sort it by name (1 means ascending order)
+  db.animals.find().sort({ name: 1 }, function(error, found) {
+    // Log any errors if the server encounters one
     if (error) {
       console.log(error);
     }
-    // Otherwise, send the doc to the browser as a json object
+    // Otherwise, send the result of this query to the browser
     else {
-      res.json(doc);
+      res.json(found);
     }
   });
 });
 
-
-// Create a new note or replace an existing note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  var newNote = new Note(req.body);
-
-  // And save the new note the db
-  newNote.save(function(error, doc) {
-    // Log any errors
+// 4. At the "/weight" path, display every entry in the animals collection, sorted by weight
+app.get("/weight", function(req, res) {
+  // Query: In our database, go to the animals collection, then "find" everything,
+  // but this time, sort it by weight (-1 means descending order)
+  db.animals.find().sort({ weight: -1 }, function(error, found) {
+    // Log any errors if the server encounters one
     if (error) {
       console.log(error);
     }
-    // Otherwise
+    // Otherwise, send the result of this query to the browser
     else {
-      // Use the article id to find and update it's note
-      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
-      // Execute the above query
-      .exec(function(err, doc) {
-        // Log any errors
-        if (err) {
-          console.log(err);
-        }
-        else {
-          // Or send the document to the browser
-          res.send(doc);
-        }
-      });
+      res.json(found);
     }
   });
 });
 
-
-// Listen on port 3000
+// Set the app to listen on port 3000
 app.listen(3000, function() {
   console.log("App running on port 3000!");
 });
